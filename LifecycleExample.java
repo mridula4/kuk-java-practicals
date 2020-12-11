a543ce42a9b201b4a85b6a08b81984c7b51dc0b5
Mridula <gabamridula@gmail.com>
2020-12-11 18:03:57 +0530

    Add files via upload
    program to illustrate all the functions of applet life cycle.

---
+++ LifecycleExample.java
@@ -0,0 +1,24 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+ /*
+<applet code="LifecycleExample" width=100 height=100>
+</applet>
+*/
+public class LifecycleExample extends Applet{
+	public void init() {
+		super.init();
+	}
+	public void start() {
+		super.start();
+	}
+	public void stop() {
+		super.stop();
+	}
+	public void paint(Graphics g) {
+		super.paint(g);
+	}
+	public void destroy() {
+		super.destroy();
+	}
+ 
+}
